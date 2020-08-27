9ef57e4271734540ee25197b64be2b2b0c74dddf
WantedHorizon <eliran.amzalag@gmail.com>
2020-08-27 19:24:21 +0300

    added red warning animation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
-const TILE_OPTION_NUMBER = 7
+const TILE_OPTION_NUMBER = 6;
+const TOOL_OPTION_NUMBER = 3;
+
 const gameTable = document.querySelector('.game-area__game');
+const lastMindedTileElement = document.querySelector('.tools-sidebar__lastMindedTile');
+const tools = document.querySelectorAll('.tool');
 
 const state = {
     gameStart: false,
     worldMatrix: [],
-    selectedTool: -1, //0-2
-    lastMindedTile: -1, //0-5
+    selectedTool: -1, // 0-pickaxe => rock(4) , 1-shovel => dirt,dirt-grass(0,1), 2-axe => leaves,wood(2,3) 
+    lastMindedTile: -1, //0-dirt , 1-dirt-grass , 2- leaves, 3- wood, 4-rock ,5-water
 }
+
 const updateLastMindedTIle = (type) => {
     state.lastMindedTile = type;
-    console.log("lastmindedtile:" + state.lastMindedTile);
+    lastMindedTileElement.dataset.type = type;
 }
-const validTool = () => true;
+//validate tool can mine element
+const validTool = (type, selectedTool) => {
+    switch (selectedTool) {
+        case 0: //p0-pickaxe => rock(4)i
+            if (type == 4) return true;
+            break;
+        case 1: // 1-shovel => dirt,dirt-grass(0,1)
+            if (type == 0 || type == 1) return true;
+            break;
+        case 2: // 2-axe => leaves,wood(2,3) 
+            if (type == 2 || type == 3) return true;
+            break;
+        default:
+            console.error("invalid tool number");
+            break;
+
+    }
+
+    return false;
+};
 
 const createWorld = (mat, tileOnClickHandler) => {
 
@@ -56,6 +80,14 @@ const createMatrix = (row, col) => {
     return mat;
 }
 
+//adds warning after invalid tool press;
+const toolwarning = () => {
+    tools.forEach( t => {
+        t.classList.add('warning');
+        setTimeout((t)=> {t.classList.remove('warning')}, 1000);
+    })
+}
+
 const tileOnClickHandler = (e) => {
     console.log(e.currentTarget.dataset.type);
     const tile = e.currentTarget;
@@ -74,6 +106,7 @@ const tileOnClickHandler = (e) => {
 
         if (!validTool(type, state.selectedTool)) {
             console.error('invalid tool');
+            toolwarning();
             return;
 
         }
@@ -94,16 +127,46 @@ const tileOnClickHandler = (e) => {
         if (state.lastMindedTile >= 0) {
             tile.dataset.type = state.lastMindedTile;
             state.lastMindedTile = -1;
+            lastMindedTileElement.dataset.type = -1;
         }
     }
 
-    console.log(state);
 
 
 
 
 }
+//resets all active tools
+const resetSelectedTools = () => {
+    tools.forEach(t => {
+        t.classList.remove('active');
+    })
+}
 
+const toolOnClickHandler = (e) => {
+    const tool = e.currentTarget;
+    const toolType = parseInt(tool.dataset.tool);
+    if (isNaN(toolType)) {
+        console.error('invalid tool type');
+        return;
+    }
+    resetSelectedTools();
+    tool.classList.add('active');
+    state.selectedTool = toolType;
+
+}
+
+
+const addEventsToTools = (toolOnClickHandler) => {
+    tools.forEach(tool => {
+        tool.addEventListener('click', toolOnClickHandler)
+    })
+}
+
+
+
+//main
+addEventsToTools(toolOnClickHandler);
 state.worldMatrix = createMatrix(12, 16);
 
 createWorld(state.worldMatrix, tileOnClickHandler);
